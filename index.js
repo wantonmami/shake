0322a1c6bf61310f1c07dc941bc489c32f294775
wantonmami <asiazer321@gmail.com>
2024-05-17 19:13:37 +0800

    shake

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var std;
 function getStandardDeviation(array) {
   const n = array.length
   const mean = array.reduce((a, b) => a + b) / n
-  return Math.sqrt(array.map(x => Math.pow(x - mean, 2)).reduce((a, b) => a + b) / n) / getAverage(arr);
+  return Math.sqrt(array.map(x => Math.pow(x - mean, 2)).reduce((a, b) => a + b) / n)
 }
 
 function getAverage(array) {
